62a6234318707d92a0ebb366fb60089ca639c12b
Harsha1309 <harshyadav1393@gmail.com>
2023-12-30 16:59:24 +0530

    updated joblib file

--- api/app.py
+++ api/app.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from joblib import load
 
-clf = load('dark_pattern_classifier.joblib')
-vectorizer = load('dark_pattern_vectorizer.joblib')
-label_encoder = load('dark_pattern_label_encoder.joblib')
+presence_classifier = load('presence_classifier.joblib')
+presence_vect = load('presence_vectorizer.joblib')
+category_classifier = load('category_classifier.joblib')
+category_vect = load('category_vectorizer.joblib')
 
 app = Flask(__name__)
 CORS(app)
@@ -16,21 +17,18 @@ def main():
         data = request.get_json().get('tokens')
 
         for token in data:
-            
-            # Vectorize the tokenized text
-            vectorized_text = vectorizer.transform([' '.join(token)])
-            # Make the prediction
-            predicted_label = clf.predict(vectorized_text)
-            # Convert the predicted label back to the original category
-            predicted_category = label_encoder.inverse_transform(predicted_label)
+            result = presence_classifier.predict(presence_vect.transform([token]))
+            if result == 'Dark':
+                cat = category_classifier.predict(category_vect.transform([token]))
+                output.append(cat[0])
+            else:
+                output.append(result[0])
 
-            output.append(predicted_category)
-
-        # dark = [data[i] for i in range(len(output)) if output[i] != 'Not Dark Pattern']
-        # for d in dark:
-        #     print(d)
-        # print()
-        # print(len(dark))
+        dark = [data[i] for i in range(len(output)) if output[i] == 'Dark']
+        for d in dark:
+            print(d)
+        print()
+        print(len(dark))
 
         message = '{ \'result\': ' + str(output) + ' }'
         print(message)
